0505da111e87912a1f9828fde2da75290584915f
rdhsaini <rdhsaini@gmail.com>
2026-03-14 18:47:53 +0400

    Fix chains for langchain 1.2

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
-
 try:
     from dotenv import load_dotenv
     load_dotenv()
